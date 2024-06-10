a212c7df1ddbd9ef4774abaef502c734b7c86074
Nathan-CSE <86046189+Nathan-CSE@users.noreply.github.com>
2024-06-10 19:38:32 +1000

    cleaned up code, implemented pattern cycling

--- jellyfish/jellyfish.ino
+++ jellyfish/jellyfish.ino
@@ -1,13 +1,10 @@
-
-// Colours: Purple (), blue (), silver (), pink () -> cool tones
-
 #include <FastLED.h>
 
-#define TOP_LED_RING    2
-#define MIDDLE_LED_RING 3
-#define BOTTOM_LED_RING 4
-#define TOP_LED         25
-#define NUM_LEDS        100
+#define TOP_LED_PIN    2
+#define MIDDLE_LED_PIN 3
+#define BOTTOM_LED_PIN 4
+#define NUM_TOP_LEDS         25
+#define NUM_MID_LEDS        100
 #define BRIGHTNESS      64
 // #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
@@ -18,8 +15,8 @@
 
 
 // Array of LEDs
-CRGB top_leds[TOP_LED];
-CRGB middle_leds[NUM_LEDS];
+CRGB top_leds[NUM_TOP_LEDS];
+CRGB middle_leds[NUM_MID_LEDS];
 
 #define UPDATES_PER_SECOND 100
  
@@ -36,8 +33,8 @@ struct LEDFade {
     bool fadingUp;
 };
 
-LEDFade topLedFades[NUM_LEDS];
-LEDFade middleLedFades[NUM_LEDS];
+LEDFade topLedFades[NUM_TOP_LEDS];
+LEDFade middleLedFades[NUM_MID_LEDS];
 
 // LED mappings used in the pew
 int starter_leds_asc[5] = {10,30,50,70,90};
@@ -51,20 +48,15 @@ CRGB* starPattern[125];
 
 void setup() {
     // power-up safety delay
-    delay( 3000 );
+    delay(3000);
     
-
     // This adds the LEDs on pin 2/3
-    FastLED.addLeds<NEOPIXEL, TOP_LED_RING>(top_leds, 25).setCorrection( TypicalLEDStrip );
-    FastLED.addLeds<NEOPIXEL, MIDDLE_LED_RING>(middle_leds, 100).setCorrection( TypicalLEDStrip );
-    // FastLED.addLeds<NEOPIXEL, BOTTOM_LED_RING>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.addLeds<NEOPIXEL, TOP_LED_PIN>(top_leds, 125).setCorrection(TypicalLEDStrip);
+    FastLED.addLeds<NEOPIXEL, MIDDLE_LED_PIN>(middle_leds, 125).setCorrection(TypicalLEDStrip);
     
-    FastLED.setBrightness(  BRIGHTNESS );
+    FastLED.setBrightness(BRIGHTNESS);
     
-    // Set the topLedFades and middleLedFades arrays to 0 
-    // memset(topLedFades, 0, sizeof(topLedFades)); 
-    // memset(middleLedFades, 0, sizeof(middleLedFades));
-
+    // Sets up the star mapping for pew effect
     setupStarMapping();
 }
 
@@ -72,14 +64,15 @@ void setup() {
 void loop()
 {    
     ChangePalettePeriodically();
-    // pew();
 
-    //Base code for elays
+    FastLED.show();
+
+    //Base code for delays
     FastLED.delay(1000 / UPDATES_PER_SECOND);
 
 }
  
-// This changes the pattern every 10 seconds.
+// This changes the pattern every 15 seconds.
 void ChangePalettePeriodically()
 {
     uint8_t secondHand = (millis() / 1000) % 60;
@@ -89,24 +82,30 @@ void ChangePalettePeriodically()
     // randomSparkle(middle_leds, middleLedFades,  100, 3, 5);
     // delay(DELAY_DURATION);
 
-    // pew();
-    // chaseEffect();
-    // rgb_snowfall();
-    snowfall(CHSV(100,0,255), CHSV(100,0,100), CHSV(100,0,50));
-    // snowfall(CRGB(255,50,255), CRGB(123,100,82), CRGB(301,100,48));
-    // twinkle();
-    // CascadingPattern();
+    if (lastSecond != secondHand) {
+        lastSecond = secondHand;
 
-    // if( lastSecond != secondHand) {
-    //     lastSecond = secondHand;
-    //     if( secondHand ==  0)  { snowfall(); }
-    //     if( secondHand ==  10) { pew(); }
-    //     if( secondHand ==  20) { chaseEffect(); }
-    //     if( secondHand ==  30) { rgb_snowfall(); }
-    //     if( secondHand ==  40) { CascadingPattern(); }
-    //     if( secondHand ==  50) { }
-    //     if( secondHand ==  60) { }
-    // }
+        if (secondHand == 0)  {
+            snowfall(CHSV(100,0,255), CHSV(100,0,100), CHSV(100,0,50));
+        }
+        
+        if (secondHand == 15) {
+            pew();
+        }
+        
+        if (secondHand == 30) {
+            chaseEffect();
+        }
+        
+        if (secondHand == 45) { 
+            randomSparkle(top_leds, topLedFades, 25, 5, 10);
+            randomSparkle(middle_leds, middleLedFades,  100, 3, 5);
+        }
+        
+        if (secondHand == 60) {
+            CascadingPattern();
+        }
+    }
 }
 
 void CascadingPattern() {
@@ -140,20 +139,6 @@ void CascadingPattern() {
     }
 }
 
-void IlluminatePalette()
-{
-    CRGB blue  = CRGB(0, 255, 20); //this is the illuminate colour, max out the green and add a bit of blue
-    // CRGB green  = CRGB::MediumAquamarine;
-    CRGB gold  = CRGB::Gold;
-    
-    currentPalette = CRGBPalette16(
-                                   blue,  blue,  blue,  blue,
-                                   blue, blue, blue,  blue,
-                                   gold,  gold,  gold,  gold,
-                                   gold, gold, gold,  gold );
-}
-
-
 void snowfall(CHSV firstColour, CHSV secondColour, CHSV thirdColour)
 {
 
@@ -165,10 +150,6 @@ void snowfall(CHSV firstColour, CHSV secondColour, CHSV thirdColour)
             for (int j = 0; j < 4; j++) { //changing four leds
                 number[j] = (die_asc[i] + t - j) % 10 + starter_leds_asc[i];
             }
-            // middle_leds[number[0]] = CHSV(100,0,255);
-            // middle_leds[number[1]] = CHSV(100,0,100);
-            // middle_leds[number[2]] = CHSV(100,0,50);
-            // middle_leds[number[3]] = CHSV(0,0,0);
 
             middle_leds[number[0]] = firstColour;
             middle_leds[number[1]] = secondColour;
@@ -181,89 +162,15 @@ void snowfall(CHSV firstColour, CHSV secondColour, CHSV thirdColour)
             for (int j = 0; j < 4; j++) {
                 number[j] = (die_desc[i] - t + j) % 10 + ender_leds_desc[i];
             }
-            // middle_leds[number[0]] = CHSV(100,0,255);
-            // middle_leds[number[1]] = CHSV(100,0,100);
-            // middle_leds[number[2]] = CHSV(100,0,50);
-            // middle_leds[number[3]] = CHSV(0,0,0);
 
             middle_leds[number[0]] = firstColour;
             middle_leds[number[1]] = secondColour;
             middle_leds[number[2]] = thirdColour;
             middle_leds[number[3]] = CHSV(0,0,0);
         }
-            
-        for (int i = 0; i < 5; i++) {
-            middle_leds[number[1]] = CHSV(100,0,100);
-            middle_leds[number[2]] = CHSV(100,0,50);
-            middle_leds[number[3]] = CHSV(0,0,0);
-        }
-
-        if (t % 5 == 0){ // THIS IS DOING TWO STEPS EVERY LED INCREMENT
-            for (int i = 0; i < 5; i++){
-
-            }
-            middle_leds[number[1]] = CHSV(100,0,100);
-            middle_leds[number[2]] = CHSV(100,0,50);
-            middle_leds[number[3]] = CHSV(0,0,0);
-        } 
-        else if (t % 5 == 1){
-        }
-        else if (t % 5 == 2){
-        }
-        else if (t % 5 == 3){
-        }
-        else if (t % 5 == 4){
-        }
-        // This code should actually be written as two for loops for 4x, for 1 to 5 (for circling middle)
-        // NOPE NOPE NOPE 20 leds because two strips 
-
-        // FOR 20 
-        // FOR 5
-
 
         FastLED.show();
         delay(100);
-    }
-}
-
-void rgb_snowfall () {
-    
-  for (int t = 0; t < 20; t++) {
-        for (int i = 0; i < 5; i++) {
-            int number[4] = {0};
-            for (int j = 0; j < 4; j++) {
-                number[j] = (die_asc[i] + t - j) % 10 + starter_leds_asc[i];
-            }
-            middle_leds[number[0]] = CRGB(255,50,255);
-            middle_leds[number[1]] = CRGB(123,100,82);
-            middle_leds[number[2]] = CRGB(301,100,48);
-            middle_leds[number[3]] = CHSV(0,0,0);
-        }
-        
-        for (int i = 0; i < 5; i++) {
-            int number[4] = {0};
-            for (int j = 0; j < 4; j++) {
-                number[j] = (die_desc[i] - t + j) % 10 + ender_leds_desc[i];
-            }
-            middle_leds[number[0]] = CRGB(255,50,255);
-            middle_leds[number[1]] = CRGB(123,100,82);
-            middle_leds[number[2]] = CRGB(301,100,48);
-            middle_leds[number[3]] = CHSV(0,0,0);
-        }
-        FastLED.show();
-        delay(100);
-    }
-}
-
-void twinkle() {
-    int array[10];
-    for (int i = 0; i < 5; i++) {
-        rand() % 10;
-        rand() % 10;
-    }
-    for (int i = 0; i < 5; i++) {
-        rand() % 10;
-        rand() % 10;
     }
 }
 
@@ -288,35 +195,12 @@ void pew() {
     }
 }
 
-
-void pew2() {
-
-    for (int x = 0; x < 250; x++) {
-
-        int a = (x + 25) % 125;  
-        int b = (x + 50) % 125;
-        int c = (x + 75) % 125;
-        int d = (x + 100) % 125;
-        int e = (x + 125) % 125;
-        
-        *starPattern[e] = CRGB::Blue; 
-        *starPattern[a] = CRGB::Purple;
-        *starPattern[b] = CRGB::Cyan;
-        *starPattern[c] = CRGB::White;
-        *starPattern[d] = CRGB(255,87,51);
-
-        FastLED.show();
-        delay(30);
-    }
-}
-
 void chaseEffect() {
   // top ring 0-24
   // mid ring 25-124
   int top_ring_len = 25;
   int middle_ring_len = 100;
 
-  // uint8_t hue = 0;
   CRGBPalette16 palette(
     // CRGB::Amethyst,
     // CRGB::LightSkyBlue,
@@ -434,4 +318,3 @@ void setupStarMapping() {
         starPattern[i] = &middle_leds[j];
     }
 }
-
